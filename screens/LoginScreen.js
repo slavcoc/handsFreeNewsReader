c3cfe1eeeced2a0ccfd78a7a04fdac1d8983aec1
slavco <cvetanovski.slavco@gmail.com>
2019-03-28 12:48:15 +0100

    added react-native-firbase dependency and module for Auth on Firebase

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,29 +1,9 @@
 import React from "react";
 import { View, Button, Text, StyleSheet } from "react-native";
-import firebase from "firebase";
-import { provider } from "../conf/firebase";
 
 class LoginScreen extends React.Component {
   test() {
-    firebase.auth().signInWithRedirect(provider);
-    firebase.auth().getRedirectResult().then(function(result) {
-      if (result.credential) {
-        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-        var token = result.credential.accessToken;
-        // ...
-      }
-      // The signed-in user info.
-      var user = result.user;
-    }).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // The email of the user's account used.
-      var email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      var credential = error.credential;
-      // ...
-    });
+    
   }
 
   render() {
